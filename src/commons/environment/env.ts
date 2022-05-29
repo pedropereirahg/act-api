0dde271337f0c57bdfaccb0fa582e654b641ffea
pedrogoncalvesk <pedrogoncalvesp.95@gmail.com>
2022-05-29 15:09:28 -0300

    fix: resolve cors problems

--- src/commons/environment/env.ts
+++ src/commons/environment/env.ts
@@ -6,7 +6,8 @@ export abstract class Env {
     process.env.NODE_ENV || 'development';
 
   public static readonly APPLICATION_PORT: number =
-    parseInt(process.env.PORT, 10) || parseInt(process.env.APPLICATION_PORT, 10);
+    parseInt(process.env.PORT, 10) ||
+    parseInt(process.env.APPLICATION_PORT, 10);
 
   public static readonly APPLICATION_VERSION: string =
     process.env.APPLICATION_VERSION;
@@ -18,7 +19,11 @@ export abstract class Env {
 
   public static readonly SWAGGER_DOCS: string = process.env.SWAGGER_DOCS;
 
-  public static readonly SWAGGER_SERVER: string = process.env.SWAGGER_SERVER;
+  public static readonly SWAGGER_SERVER: string[] =
+    process.env.SWAGGER_SERVER &&
+    Array.isArray(process.env.SWAGGER_SERVER.split(','))
+      ? process.env.SWAGGER_SERVER.split(',')
+      : [];
 
   public static readonly DATABASE_URI: string = process.env.DATABASE_URI;
 }
